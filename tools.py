af6b34466e67553f11c4a3914c33634dc2a37229
hetang <505888379@qq.com>
2020-07-28 16:08:22 +0800

    尚未完成的任务： data_pre-process.ipynb：自制标准化与归一化，并与sklearn库作比较

--- tools.py
+++ tools.py
@@ -1,7 +1,6 @@
-import os
-import glob
-import random
-import shutil
+import pandas as pd
+import numpy as np
+
 
 # 先写测试脚本，然后封装为方法
 # 脚本1 - 分割数据集
@@ -10,3 +9,23 @@ import shutil
 
 
 # 数据预处理 - 采用sklearn
+
+def LinkData(filepath): # 拼接数据
+    """
+    从filepath中读取csv文件，拼接，返回dataframe.values
+    """
+    csvs = []
+    fh = open(filepath, 'r')
+    for line in fh:
+        line = line.rstrip()
+        paths = line.split()
+        csvs.append(paths[0])
+    
+    for csv in csvs:
+        data = pd.read_csv(csv, header=None)
+        if(csv == csvs[0]):
+            ret = data
+        else:
+            ret = pd.concat([ret, data], ignore_index=True)
+    
+    return ret.values        
